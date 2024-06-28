f114f7d141ca0d8d75ca0f82490e5ce09ad170e4
Arturo Rodriguez <davidarturo9911@hotmail.com>
2024-06-28 00:40:40 -0500

    Carga desde archivo!

--- main.js
+++ main.js
@@ -2,43 +2,35 @@ import { PhotoView, TitleView, MosaicView } from "./js/PhotoSlide.js";
 
 const parent = document.getElementById("page_content");
 
-const content = {};
+fetch("./resources/main.json")
+	.then((response) => response.json())
+	.then((json) => parse_data(json));
 
-const el_content = document.createElement("div");
-el_content.classList.add("top-left");
-el_content.innerHTML =
-	"<p>Estamos construyendo la experiencia</p><p>El viernes estará terminado</p>";
+function parse_data(json) {
+	document.title = json.title;
+	for (const page of json.content) {
+		const type = page.type;
+		const content = page.content;
+		if (type == "title_view") {
+			new TitleView(parent, content.title, content.subtitles);
+		} else if (type == "photo_view") {
+			const img_src = page.img_src;
+			const el_content = document.createElement("div");
+			el_content.classList.add("PhotoView_content");
 
-content["main"] = { type: "html", content: el_content };
-content["main"] = {
-	type: "text",
-	content:
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est odio, condimentum et purus in, sollicitudin tristique eros. Pellentesque hendrerit lobortis ex nec maximus. In hac habitasse platea dictumst. In molestie eget ipsum quis pulvinar. In aliquam nibh id bibendum aliquam. Sed auctor massa erat, in vulputate arcu volutpat sed. Aliquam efficitur enim gravida, vehicula lacus in, semper orci. Etiam lacinia maximus congue. \n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est odio, condimentum et purus in, sollicitudin tristique eros. Pellentesque hendrerit lobortis ex nec maximus. In hac habitasse platea dictumst. In molestie eget ipsum quis pulvinar. In aliquam nibh id bibendum aliquam. Sed auctor massa erat, in vulputate arcu volutpat sed. Aliquam efficitur enim gravida, vehicula lacus in, semper orci. Etiam lacinia maximus congue.",
-};
-content["camera"] = "Vivitar 3000 - Fujifilm 200";
-content["location"] = "Lisboa, Portugal. 2 Jun, 2024";
-const img_src = "WIPBG.jpg";
-const title = new TitleView(parent, "BOGOTÁ D.C.", [
-	"28/5/24 - 1/6/24",
-	"Población 9' habitantes",
-]);
-const wip_page = new PhotoView(parent, content, img_src);
-const title2 = new TitleView(parent, "LISBOA, PORTUGAL", ["28/5/24 - 1/6/24"]);
-const wip_page2 = new PhotoView(parent, content, img_src);
-const title3 = new TitleView(parent, "MADRID, ESPAÑA", ["28/5/24 - 1/6/24"]);
-const wip_page3 = new PhotoView(parent, content, img_src);
-const mos_page3 = new MosaicView(parent, [
-	{ src: "WIPBG.jpg", description: "sample" },
-	{ src: "WIPBG.jpg", description: "sample" },
-]);
-const mos_page4 = new MosaicView(parent, [
-	{ src: "WIPBG.jpg", description: "sample" },
-	{ src: "WIPBG.jpg", description: "sample" },
-	{ src: "WIPBG.jpg", description: "sample" },
-	{ src: "WIPBG.jpg", description: "sample" },
-]);
-const mos_page5 = new MosaicView(parent, [
-	{ src: "WIPBG.jpg", description: "sample" },
-	{ src: "WIPBG.jpg", description: "sample" },
-	{ src: "WIPBG.jpg", description: "sample" },
-]);
+			var text = "";
+			for (const con of page.contents) {
+				text += `<h3>${con.title}</h3><p>${con.text}</p><br>`;
+			}
+
+			el_content.innerHTML = text;
+			const parsed_content = {};
+			parsed_content["main"] = { type: "html", content: el_content };
+			parsed_content["camera"] = page.camera;
+			parsed_content["location"] = page.location;
+			new PhotoView(parent, parsed_content, img_src);
+		} else {
+			new MosaicView(parent, page.imgs);
+		}
+	}
+}
